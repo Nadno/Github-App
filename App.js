7692c5c07a7026e575ac150e4900b82739369db4
Nadno <eunadno@gmail.com>
2022-08-12 07:30:34 -0300

    feat(App): add Details screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './screen/Home';
+import Details from './screen/Details';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Details" component={Details}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
